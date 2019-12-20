5547c91041eae667dbd2abc90a58d03c267cf85d
Bweya Omukhulu <bweya@Bweyas-MacBook-Air.local>
2019-12-19 22:43:50 -0500

    Remove hours, min, seconds from dates 19-DEC-2019

--- T2.py
+++ T2.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 
 import datetime
 import os
-def trade(d):
-    filter_column = 'Period after CO create date'
+def trade():
+    filter_column = 'Period after CO value date'
     DateNow = datetime.datetime.now()
     day = DateNow.strftime("%d")
     month = DateNow.strftime("%b")
@@ -24,7 +24,7 @@ def trade(d):
         if f[:6] == 'FX-133':
             FX133TradeData = pd.read_excel("files/"+f, sheet_name = 0, header = None, skiprows=1)
 
-    month_dict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+    month_dict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'June':6, 'July':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
 
     #Get indices of approved records
 
@@ -99,7 +99,6 @@ def trade(d):
         count_EUR_index+=1
 
 
-
     #=======================================================
 
     #=======================USD
@@ -143,7 +142,7 @@ def trade(d):
 
         trade_writer = csv.writer(output, delimiter = ',')
         trade_writer.writerow(['A','B','C','D','E','F','G', 'H', 'I', 'J','K', 'L' ] )
-        trade_writer.writerow(['Index','CO Request ID','Creation Date','FX Deal No','Deal Amount','Currency','Value Date HQ', 'Value Date 133', 'Business Partner', filter_column,'Add 3 days number; Average number of days not received by CO', 'Trader' ] )
+        trade_writer.writerow(['Item No.','CO Request ID','Creation Date','Value Date CO','FX Deal No','Deal Amount','Currency', 'Value Date HQ', 'Business Partner', filter_column,'Add 3 days number; Average number of days not received by CO', 'Trader' ] )
         j = 0
         count_record = 1
         for y in DealNumber:
@@ -151,35 +150,67 @@ def trade(d):
             for x in usdIndex_get_DealNumbers:
                 date_133US = FX133TradeData[32][x].replace(".","/");
                 date_133USobj = datetime.datetime.strptime(date_133US, '%d/%m/%Y')
-                if y == FX133TradeData[48][x] and HqTradeData[7][j][:3] != 'DKK' and (date_133USobj-HqTradeData[15][j]) >= timedelta(days = d):
-                    if HqTradeData[15][j].month == (month_dict[getmonth] - 1):
-                        trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j],str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],TradeData[9][approved_index[j]],date_133USobj,FX133TradeData[41][x],(date_133USobj-HqTradeData[15][j]),((date_133USobj-HqTradeData[15][j])),FX133TradeData[3][x] ])
-                        #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
-                        ALL_indices.append(j)
-                        getPartners.append( FX133TradeData[41][x] )
-                        count_record+=1
+                if y == FX133TradeData[48][x] and HqTradeData[7][j][:3] != 'DKK':
+                    #and (TradeData[9][approved_index[j]]-date_133USobj) >= timedelta(days = d):
+                    periodUS = (date_133USobj-TradeData[9][approved_index[j]]).days
+                    if month_dict[getmonth] == 1:
+                        if TradeData[9][approved_index[j]].month == 12:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133USobj.date(),FX133TradeData[41][x],str(periodUS)+' days',str(periodUS)+' days',FX133TradeData[3][x] ])
+                            #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
 
+                        else:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133USobj.date() ,FX133TradeData[41][x],str(periodUS)+' days',str(periodUS+3)+' days',FX133TradeData[3][x] ])
+                            #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
                     else:
-                        trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j],str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],TradeData[9][approved_index[j]],date_133USobj ,FX133TradeData[41][x],(date_133USobj-HqTradeData[15][j]),((date_133USobj-HqTradeData[15][j])+timedelta(days=3)),FX133TradeData[3][x] ])
-                        #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
-                        ALL_indices.append(j)
-                        getPartners.append( FX133TradeData[41][x] )
-                        count_record+=1
+                        if TradeData[9][approved_index[j]].month == (month_dict[getmonth] - 1):
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133USobj.date(),FX133TradeData[41][x],str(periodUS)+ ' days',str(periodUS)+' days',FX133TradeData[3][x] ])
+                            #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
+
+                        else:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133USobj.date() ,FX133TradeData[41][x],str(periodUS)+ ' days',str(periodUS+3)+' days',FX133TradeData[3][x] ])
+                            #print(HqTradeData[0][j], TradeData[9][approved_index[j]]-date_133USobj,' : ',HqTradeData[7][j][:3])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
 
             for x in EURIndex_get_DealNumbers:
                 date_133EU = FX133TradeData[32][x].replace(".","/");
                 date_133EUobj = datetime.datetime.strptime(date_133EU, '%d/%m/%Y')
-                if y == FX133TradeData[48][x] and HqTradeData[7][j][:3] != 'DKK' and (date_133EUobj-HqTradeData[15][j]) >= timedelta(days = d):
-                    if HqTradeData[15][j].month == (month_dict[getmonth] - 1):
-                        trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j],str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],TradeData[9][approved_index[j]],date_133EUobj,FX133TradeData[41][x],(date_133EUobj-HqTradeData[15][j]),((date_133EUobj-HqTradeData[15][j])),FX133TradeData[3][x] ])
-                        ALL_indices.append(j)
-                        getPartners.append( FX133TradeData[41][x] )
-                        count_record+=1
+                if y == FX133TradeData[48][x] and HqTradeData[7][j][:3] != 'DKK':
+                #and (TradeData[9][approved_index[j]]-date_133EUobj) >= timedelta(days = d):
+                    periodEU = (date_133EUobj-TradeData[9][approved_index[j]]).days
+                    if month_dict[getmonth] == 1:
+
+                        if TradeData[9][approved_index[j]].month == 12:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133EUobj.date(),FX133TradeData[41][x],str(periodEU)+' days',str(periodEU)+' days',FX133TradeData[3][x] ])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
+                        else:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133EUobj.date(),FX133TradeData[41][x],str(periodEU)+' days',str(periodEU+3)+' days',FX133TradeData[3][x] ])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
                     else:
-                        trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j],str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],TradeData[9][approved_index[j]],date_133EUobj,FX133TradeData[41][x],(date_133EUobj-HqTradeData[15][j]),((date_133EUobj-HqTradeData[15][j])+timedelta(days=3)),FX133TradeData[3][x] ])
-                        ALL_indices.append(j)
-                        getPartners.append( FX133TradeData[41][x] )
-                        count_record+=1
+                        if TradeData[9][approved_index[j]].month == (month_dict[getmonth] - 1):
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133EUobj.date(),FX133TradeData[41][x],str(periodEU)+' days',str(periodEU)+' days',FX133TradeData[3][x] ])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
+                        else:
+                            trade_writer.writerow([count_record, HqTradeData[0][j],HqTradeData[15][j].date(),TradeData[9][approved_index[j]].date(),str(y),"{:,.2f}".format(HqTradeData[9][j]),HqTradeData[7][j],date_133EUobj.date(),FX133TradeData[41][x],str(periodEU)+' days',str(periodEU+3)+' days',FX133TradeData[3][x] ])
+                            ALL_indices.append(j)
+                            getPartners.append( FX133TradeData[41][x] )
+                            count_record+=1
 
 
             j+=1
